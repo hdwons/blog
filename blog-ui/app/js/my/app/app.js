3362bad2de5fccf13a421b845c968882f687f5b7
hdwons <hdwons@163.com>
2016-04-30 15:00:23 +0800

    basic structure

---
+++ blog-ui/app/js/my/app/app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by donghe on 16/4/18.
+ */
+var app = angular.module("blogAPP", []);
---
+++ blog-ui/app/js/my/controller/index/index.js
@@ -0,0 +1,6 @@
+/**
+ * Created by donghe on 16/4/18.
+ */
+app.controller("indexCtrl", function ($scope) {
+    $scope.name = "hdzy";
+});
